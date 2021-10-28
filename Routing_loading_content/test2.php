c52225ef0db40b1a8a57539300e47998d8b9de65
phuongtrieu97coder <82598726+phuongtrieu97coder@users.noreply.github.com>
2021-10-28 13:00:59 -0700

    Add files via upload

---
+++ Routing_loading_content/test2.php
@@ -0,0 +1,98 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <head>
+     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+
+     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js"></script>
+
+
+
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+     
+     <title>PHUONG WEB</title>
+
+     <link rel="shortcut icon"   href="image/js_logo.png" type="image/x-icon">
+
+      <link rel="stylesheet" type="text/css" href="Sass_Css_file/test2.css">
+           
+  
+
+
+         <style>
+       
+
+
+          
+           
+         </style>
+    </head>
+
+
+    <body>
+
+      <header>
+    
+      </header>
+
+     <main>
+
+     
+    
+  
+<div id="block1">
+   <img src="image/logo6.jpg" alt="logo6" width="100%" height="300px">
+</div>
+
+
+
+      
+      
+
+    
+
+   
+
+
+     </main>
+
+ 
+
+   
+     
+      
+    </body>
+
+
+   
+
+
+    <footer>
+
+
+    </footer>
+
+
+
+
+</html>
+        
+
+
+
+
+
+
+
+
